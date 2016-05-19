65aa0ab8024f911a9d1d74f272d9239fa46852b1
dvbencomo <dvbencomo@gmail.com>
2016-05-19 19:53:24 +0100

    Integración con Spring Security

---
+++ src/main/java/com/treexor/springmvc/configuration/SecurityInit.java
@@ -0,0 +1,7 @@
+package com.treexor.springmvc.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+
+}
